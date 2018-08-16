a1c1a6b09e64ac410061cd622da98bb5c2aaeed7
Jeff McCarrell <jeff@mccarrell.org>
2018-08-16 13:38:55 -0700

    add json weather parsing to the app.

--- sbt-play/foo-build/src/main/scala/example/Hello.scala
+++ sbt-play/foo-build/src/main/scala/example/Hello.scala
@@ -1,5 +1,10 @@
 package example
 
+import scala.concurrent._, duration._
+import core.Weather
+
 object Hello extends App {
-  println("Hello")
+  val w = Await.result(Weather.weather, 10.seconds)
+  println(s"Hello.  the weather in San Jose is $w.")
+  Weather.http.close()
 }
